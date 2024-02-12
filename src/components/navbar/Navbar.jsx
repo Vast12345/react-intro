ea351c4603a2d0729175915d6579c7360f733647
EnzoLopezMendez <143453673+Vast12345@users.noreply.github.com>
2024-02-12 21:59:05 +0000

    Adding more to the navbar (Unfinished)

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react'
-import { riMenu3Line, RiCloseLin } from 'react-icons/ri';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css'
 import logo from '../../assets/GPT-3.svg';
 // BEM => Block Element Modifier
 
+const Menu = () => (
+  <>
+    <p><a href='#home'>Home</a></p>
+    <p><a href='#wgpt3'>What is GPT3?</a></p>
+    <p><a href='#possibility'>Open AI</a></p>
+    <p><a href='#features'>Case Studies</a></p>
+    <p><a href='#blog'>Library</a></p>
+  </>
+)
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
@@ -14,11 +23,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' />
         </div>
         <div className='gpt3__navbar-links_container'>
-          <p><a href='#home'>Home</a></p>
-          <p><a href='#wgpt3'>What is GPT3?</a></p>
-          <p><a href='#possibility'>Open AI</a></p>
-          <p><a href='#features'>Case Studies</a></p>
-          <p><a href='#blog'>Library</a></p>
+          <Menu />
         </div>
       </div>
       <div className='gpt3__navbar-sign'>
@@ -26,7 +31,21 @@ const Navbar = () => {
         <button type='button'>Sign Up</button>
       </div>
       <div className='gpt3__navbar-menu'>
-      
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className="gpt3_navbar-menu_container scale-up-center">
+            <div className="gpt3_navbar-menu_container-links">
+              <Menu />
+              <div className='gpt3__navbar-menu_container-links-sign'>
+                <p>Sign in</p>
+                <button type='button'>Sign Up</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
